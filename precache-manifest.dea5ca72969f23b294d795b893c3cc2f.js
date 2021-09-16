9444cc4bda062c06d7e995db33b53b5855e47371
Sunil Dutt <dutt447@gmail.com>
2021-09-16 11:04:20 +0530

    Updates

---
+++ precache-manifest.dea5ca72969f23b294d795b893c3cc2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381e18885a86a89cc6c921f6607fcd02",
+    "url": "/morse/index.html"
+  },
+  {
+    "revision": "20bc186261e62becbef7",
+    "url": "/morse/static/css/main.8d63c56d.chunk.css"
+  },
+  {
+    "revision": "b420660782789fba18df",
+    "url": "/morse/static/js/2.cb350622.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/morse/static/js/2.cb350622.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20bc186261e62becbef7",
+    "url": "/morse/static/js/main.1c12a42a.chunk.js"
+  },
+  {
+    "revision": "c2cc4e4844b635942751",
+    "url": "/morse/static/js/runtime-main.33646139.js"
+  },
+  {
+    "revision": "cd2cf56081f8fca549078d7e060dce08",
+    "url": "/morse/static/media/electronic_key.cd2cf560.jpg"
+  },
+  {
+    "revision": "f140bc7b0ff7580d795ffeaac255d0c9",
+    "url": "/morse/static/media/straight_key.f140bc7b.jpg"
+  }
+]);
